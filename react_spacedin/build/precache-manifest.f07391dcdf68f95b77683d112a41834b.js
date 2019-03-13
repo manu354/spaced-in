37ecc7235d3fcd5ef1b346c54dd5650f9dda18d4
Manu Masson <manummasson8@gmail.com>
2019-03-13 19:06:16 +1100

    NoKeep mode'

---
+++ react_spacedin/build/precache-manifest.f07391dcdf68f95b77683d112a41834b.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ed12ca430db89e02f473502648eadc5",
+    "url": "/static/media/sprite-flags-24x24.7ed12ca4.png"
+  },
+  {
+    "revision": "fbee1caa6d9aad16fb0a",
+    "url": "/static/js/0.fbee1caa.chunk.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "272ad889c1508c3b1447",
+    "url": "/static/js/main.272ad889.chunk.js"
+  },
+  {
+    "revision": "adc3fbed425a515db56b",
+    "url": "/static/js/3.adc3fbed.chunk.js"
+  },
+  {
+    "revision": "9f7656e72fbed1604727",
+    "url": "/static/js/4.9f7656e7.chunk.js"
+  },
+  {
+    "revision": "7adf21eae8adb6da0b5e",
+    "url": "/static/js/5.7adf21ea.chunk.js"
+  },
+  {
+    "revision": "8fa54ae39be327924c2f",
+    "url": "/static/js/6.8fa54ae3.chunk.js"
+  },
+  {
+    "revision": "cedb624ff3507ca97ebd",
+    "url": "/static/js/7.cedb624f.chunk.js"
+  },
+  {
+    "revision": "86a73ba877246516865f",
+    "url": "/static/js/8.86a73ba8.chunk.js"
+  },
+  {
+    "revision": "d40f9cdad310d3bf0988",
+    "url": "/static/js/9.d40f9cda.chunk.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "7bb16d92163815571cb8",
+    "url": "/static/js/10.7bb16d92.chunk.js"
+  },
+  {
+    "revision": "d4993fd9dc2a9af4d8f3",
+    "url": "/static/js/11.d4993fd9.chunk.js"
+  },
+  {
+    "revision": "6bd4cbd28dcf5617cc83",
+    "url": "/static/js/12.6bd4cbd2.chunk.js"
+  },
+  {
+    "revision": "8bb02edfce7fc319350b",
+    "url": "/static/js/13.8bb02edf.chunk.js"
+  },
+  {
+    "revision": "4781c9725300afcd8b49",
+    "url": "/static/js/runtime~main.4781c972.js"
+  },
+  {
+    "revision": "40e7a395e948a8308516",
+    "url": "/static/js/1.40e7a395.chunk.js"
+  },
+  {
+    "revision": "a4d31128b633bc0b1cc1f18a34fb3851",
+    "url": "/static/media/Material-Design-Iconic-Font.a4d31128.woff2"
+  },
+  {
+    "revision": "d2a55d331bdd1a7ea97a8a1fbb3c569c",
+    "url": "/static/media/Material-Design-Iconic-Font.d2a55d33.woff"
+  },
+  {
+    "revision": "b351bd62abcd96e924d9f44a3da169a7",
+    "url": "/static/media/Material-Design-Iconic-Font.b351bd62.ttf"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/notification.d41d8cd9.woff"
+  },
+  {
+    "revision": "397dc577ed44baed37b37b7d7adba7df",
+    "url": "/static/media/notification.397dc577.svg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/notification.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/notification.d41d8cd9.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "272ad889c1508c3b1447",
+    "url": "/static/css/main.711fe9da.chunk.css"
+  },
+  {
+    "revision": "7bb16d92163815571cb8",
+    "url": "/static/css/10.54fe27ec.chunk.css"
+  },
+  {
+    "revision": "9f61e8edcbf47f921ca2f4e361d51a40",
+    "url": "/index.html"
+  }
+];
